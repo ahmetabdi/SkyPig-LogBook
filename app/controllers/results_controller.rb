354e6f58fa4cc9f2d6df52d9053991b377c0ab9d
Ahmet <ahmetabdi@gmail.com>
2012-12-13 18:29:33 +0000

    Initial Commit

---
+++ app/controllers/results_controller.rb
@@ -0,0 +1,27 @@
+class ResultsController < ApplicationController
+	require 'giantbomb'
+	require 'will_paginate/array'
+
+	def index
+		unless (params[:search] rescue nil).nil?
+			@games = GiantBomb::Game.find(params[:search])
+			@results =  @games.paginate(:page => params[:page], :per_page => 6)
+		end
+		if @results.empty? || @results.nil?
+			redirect_to :back
+			flash[:error] = "No search results could be found."
+		end
+		
+
+		#@page_results = @results.paginate(page: params[:page])
+		#@games = @game.to_json
+		#@games = @game.as_json
+	end
+
+	def show
+		@game = GiantBomb::Game.find(params[:game])
+		@game = @game.first
+	end
+
+
+end
